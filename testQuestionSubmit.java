ac23587a1400f2a1df8feb288a1e5c1142eb3ad1
jda34 <jda34@pitt.edu>
2015-11-21 13:43:41 -0500

    updated junit test to test for login and question submission

--- testQuestionSubmit.java
+++ testQuestionSubmit.java
@@ -1,4 +1,4 @@
-package com.example.tests;
+package cityquizdb;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
@@ -10,67 +10,82 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TestQuestionSubmit {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://www.cityquizsubmit.appspot.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
+		baseUrl = "http://www.cityquizsubmit.appspot.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void testQuestionSubmit() throws Exception {
-    driver.get(baseUrl + "/");
-    driver.findElement(By.id("questionbox")).clear();
-    driver.findElement(By.id("questionbox")).sendKeys("Is this a question?");
-    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
-	String URL = driver.getCurrentUrl();
-	assertEquals(URL, "http://www.cityquizsubmit.appspot.com/savePost");
-  }
+	@Test
+	//tests that the login works and question submission works
+	public void testQuestionSubmit() throws Exception {
+		driver.get(baseUrl + "/");
+		driver.findElement(By.id("username")).clear();
+		driver.findElement(By.id("username")).sendKeys("DannyA");
+		driver.findElement(By.id("password")).clear();
+		driver.findElement(By.id("password")).sendKeys("123");
+		driver.findElement(By.id("loginsubmit")).click();
+		new Select(driver.findElement(By.id("catagories"))).selectByVisibleText("Business");
+		driver.findElement(By.id("questionbox")).clear();
+		driver.findElement(By.id("questionbox")).sendKeys("test");
+		driver.findElement(By.id("answer1")).clear();
+		driver.findElement(By.id("answer1")).sendKeys("1");
+		driver.findElement(By.id("answer2")).clear();
+		driver.findElement(By.id("answer2")).sendKeys("2");
+		driver.findElement(By.id("answer3")).clear();
+		driver.findElement(By.id("answer3")).sendKeys("3");
+		driver.findElement(By.id("answer4")).clear();
+		driver.findElement(By.id("answer4")).sendKeys("4");
+		driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+		String URL = driver.getCurrentUrl();
+		assertEquals(URL, "http://www.cityquizsubmit.appspot.com/savePost");
+	}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
 }
